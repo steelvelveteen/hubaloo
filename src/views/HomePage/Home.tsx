e19913d02447ba3fac6f51dfd74c554de0a91ff1
steelvelveteen <joeyvico@gmail.com>
2020-09-21 13:47:36 +0930

    Added axios and api call to develop branch

--- src/views/HomePage/Home.tsx
+++ src/views/HomePage/Home.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const HomePage = (): React.ReactElement => (
-    <div>
-        <div>Inside the home main page</div>
-    </div>
-);
+import axios, { AxiosResponse } from 'axios';
+
+// import DataService from '../../services/data.service';
+
+type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+const HomePage = (): React.ReactElement => {
+    const [posts, setPosts] = React.useState<Post[]>();
+    React.useEffect(() => {
+        axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+            .then((response: AxiosResponse) => {
+                setPosts(response.data);
+            });
+    }, []);
+
+    return (
+        <div>
+            <div>Inside the home main page</div>
+            {posts?.map((post: Post) => <div>{post.body}</div>)}
+        </div>
+    )
+};
 
 export default HomePage;
