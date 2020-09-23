7fd3efeb3da937c200e074104cac203c29932caa
steelvelveteen <joeyvico@gmail.com>
2020-09-23 11:05:09 +0930

    Returned to origin state for develop branch

--- src/views/HomePage/Home.tsx
+++ src/views/HomePage/Home.tsx
@@ -1,30 +1,9 @@
 import React from 'react';
 
-import axios, { AxiosResponse } from 'axios';
-
-// import DataService from '../../services/data.service';
-
-type Post = {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-}
-const HomePage = (): React.ReactElement => {
-    const [posts, setPosts] = React.useState<Post[]>();
-    React.useEffect(() => {
-        axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts')
-            .then((response: AxiosResponse) => {
-                setPosts(response.data);
-            });
-    }, []);
-
-    return (
-        <div>
-            <div>Inside the home main page</div>
-            {posts?.map((post: Post) => <div>{post.body}</div>)}
-        </div>
-    )
-};
+const HomePage = (): React.ReactElement => (
+    <div>
+        <div>Inside the home main page</div>
+    </div>
+);
 
 export default HomePage;
