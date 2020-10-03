d2f410a62cc959374ee8fa4c7495ccd577ac3765
steelvelveteen <joeyvico@gmail.com>
2020-10-04 10:21:59 +1030

    Added cors on backend and rendering mongo users in front end

--- src/views/ProfilePage/Profile.tsx
+++ src/views/ProfilePage/Profile.tsx
@@ -1,13 +1,16 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
 
 import { AxiosResponse } from 'axios';
 import { map } from 'rxjs/operators';
 
-import { GetUsers } from '../../services/data.service';
-import { TUser } from '../../types/Types';
+import { GetUsers, GetUsersFromMyAPI } from '../../services/data.service';
+import { TMongoUser, TUser } from '../../types/Types';
 
 const ProfilePage: React.FC = () => {
     const [users, setUsers] = React.useState<TUser[]>();
+    const [mongoUsers, setMongoUsers] = React.useState<TMongoUser[]>();
+
     React.useEffect(() => {
         const subscription = GetUsers()
             .pipe(
@@ -20,10 +23,25 @@ const ProfilePage: React.FC = () => {
         return () => subscription.unsubscribe();
     }, []);
 
+    React.useEffect(() => {
+        const subscription = GetUsersFromMyAPI()
+            .pipe(
+                map((response: AxiosResponse) => response.data.users),
+            )
+            .subscribe((response: TMongoUser[]) => {
+                console.log(response);
+                setMongoUsers(response)
+            });
+
+        return () => subscription.unsubscribe();
+    }, []);
     return (
         <>
             <h3>Inside the profile page displaying users available from jsonplaceholder.com</h3>
             { users && users.map((user: TUser) => <div key={user.id}>{user.username}</div>)}
+
+            <h3>Users that come from my api Heroku + ATLAS</h3>
+            { mongoUsers && mongoUsers.map((user: TMongoUser) => <div key={user._id}>{user.email}</div>)}
         </>
     );
 };
