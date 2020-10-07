4326faecbdf7a224a95b3d00dbcba464652da612
steelvelveteen <joeyvico@gmail.com>
2020-10-08 10:01:26 +1030

    Multiple urls for heroku, local and local docker easily interchangeable

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -6,8 +6,12 @@ import { TCredentials, TMongoUser, TPost } from '../types/Types';
 const token = 'eyJhbGciOiJIUzI1NiJ9.am9leXZpY29AZ21haWwuY29t.q3KyV9cSOkoa_JUiCBVDhzTltyA8GWyLC3evK8Ek6gE';
 
 const herokuApiUrl = 'https://stormy-garden-32374.herokuapp.com';
-const localApiUrl = 'http://localhost:4000';
+const localApiUrl = 'http://localhost:8080';
 const dockerlocalApiUrl = 'http://localhost:49160';
+
+// const baseURL = localApiUrl;
+// const baseURL = herokuApiUrl;
+const baseURL = dockerlocalApiUrl;
 
 const authAxios: AxiosInstance = axios.create({
     baseURL: herokuApiUrl,
@@ -21,9 +25,9 @@ const GetPosts = (): Observable<AxiosResponse<TPost[]>> => from(axios.get<TPost[
 const GetUsersFromMyAPI = (): Observable<AxiosResponse<TMongoUser[]>> => from(authAxios.get<TMongoUser[]>('/users'));
 
 // eslint-disable-next-line max-len
-const LoginUser = (credentials: TCredentials): Observable<AxiosResponse<TMongoUser>> => from(axios.post<TMongoUser>(`${herokuApiUrl}/users/login`, credentials));
+const LoginUser = (credentials: TCredentials): Observable<AxiosResponse<TMongoUser>> => from(axios.post<TMongoUser>(`${baseURL}/users/login`, credentials));
 
-const SignupUser = (credentials: TCredentials): Observable<AxiosResponse<TMongoUser>> => from(axios.post<TMongoUser>(`${herokuApiUrl}/users/signup`, credentials));
+const SignupUser = (credentials: TCredentials): Observable<AxiosResponse<TMongoUser>> => from(axios.post<TMongoUser>(`${baseURL}/users/signup`, credentials));
 
 export {
     GetPosts, GetUsersFromMyAPI, LoginUser, SignupUser
