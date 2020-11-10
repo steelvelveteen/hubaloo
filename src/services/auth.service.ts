3bb14ad4899be6ff10eccbeb5250eed0dab0fcc0
steelvelveteen <joeyvico@gmail.com>
2020-11-10 11:32:35 +1030

    Refactoring, normalizing naming convention for services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,15 +2,15 @@ import { AxiosResponse } from 'axios';
 import { Observable } from 'rxjs';
 
 import { CredentialsType, ResetCredentialsType, UserType } from '../types/Types';
-import * as DataService from './data.service';
+import * as _dataService from './data.service';
 
 const emailFormat = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const Login = (credentials: CredentialsType): Observable<AxiosResponse<UserType>> => DataService.Post(credentials, '/auth/login');
+const Login = (credentials: CredentialsType): Observable<AxiosResponse<UserType>> => _dataService.Post(credentials, '/auth/login');
 
-const SignUp = (credentials: CredentialsType): Observable<AxiosResponse<UserType>> => DataService.Post(credentials, '/auth/signup');
+const SignUp = (credentials: CredentialsType): Observable<AxiosResponse<UserType>> => _dataService.Post(credentials, '/auth/signup');
 
-const ResetPassword = (credentials: ResetCredentialsType): Observable<AxiosResponse<UserType>> => DataService.Post(credentials, '/auth/resetPassword');
+const ResetPassword = (credentials: ResetCredentialsType): Observable<AxiosResponse<UserType>> => _dataService.Post(credentials, '/auth/resetPassword');
 
 const validateEmail = (email: string): RegExpMatchArray | null => email.match(emailFormat);
 
